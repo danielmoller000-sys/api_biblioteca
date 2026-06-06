a14a34409eb8590649754dd422cc7de7c275ebdb
daniel <danielmoller000@gmail.com>
2026-06-06 15:35:59 +0100

    crea AutorService con el CRUD y la lógica de negocio

---
+++ src/main/java/com/example/api_biblioteca/Service/AutorService.java
@@ -0,0 +1,32 @@
+package com.example.api_biblioteca.Service;
+
+import com.example.api_biblioteca.model.Autor;
+import com.example.api_biblioteca.Repository.AutorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AutorService {
+
+    private final AutorRepository autorRepository;
+
+    public List<Autor> findAll() {
+        return autorRepository.findAll();
+    }
+
+    public Optional<Autor> findById(Long id) {
+        return autorRepository.findById(id);
+    }
+
+    public Autor save(Autor autor) {
+        return autorRepository.save(autor);
+    }
+
+    public void deleteById(Long id) {
+        autorRepository.deleteById(id);
+    }
+}
